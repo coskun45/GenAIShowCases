61e270230f20b482fac7af13651ae7d3d9f2224f
Eyüp <coskuney85480@th-nuernberg.de>
2024-10-14 16:32:17 +0200

    resquirements frei

--- helperClass/llms.py
+++ helperClass/llms.py
@@ -9,18 +9,37 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_together import ChatTogether
-import credentials
-def get_azure_openAI():
-    azure_llm = AzureChatOpenAI(
-            deployment_name=credentials.MODEL_NAME,
+from dotenv import load_dotenv
+
+# .env dosyasını yükle
+load_dotenv()
+
+
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+TOGETHER_API_KEY = os.getenv('TOGETHER_API_KEY')
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+
+# def get_azure_openAI():
+#     azure_llm = AzureChatOpenAI(
+#             deployment_name=AZURE_MODEL_NAME,
+#             temperature=0,
+#             max_tokens=None,
+#             api_version=AZURE_API_VERSION,
+#             azure_endpoint =AZURE_OPENAI_ENDPOINT,
+#             api_key=AZURE_OPENAI_API_KEY,
+
+#         )
+#     return azure_llm
+
+def get_openAI():
+    openAI_llm = ChatOpenAI(
             temperature=0,
-            max_tokens=None,
-            api_version=credentials.API_VERSION,
-            azure_endpoint =credentials.AZURE_OPENAI_ENDPOINT,
-            api_key=credentials.OPENAI_API_KEY,
+            api_key=OPENAI_KEY,
 
         )
-    return azure_llm
+    return openAI_llm
 
 def google_gemini():
     client = ChatGoogleGenerativeAI(
@@ -29,7 +48,7 @@ def google_gemini():
             max_tokens=None,
             timeout=None,
             max_retries=2,
-            api_key=credentials.GOOGLE_API_KEY,
+            api_key=GOOGLE_API_KEY,
         )
     return client
 
@@ -41,6 +60,6 @@ def llama3_405B():
         max_tokens=None,
         timeout=None,
         max_retries=2,
-        api_key=credentials.TOGETHER_API_KEY,
+        api_key=TOGETHER_API_KEY,
     )
     return client
